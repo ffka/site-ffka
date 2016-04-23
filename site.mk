53792b70fe0f553effccb929ef36e6bcff7935ff
herrbett <simon@terzenba.ch>
2016-04-23 20:53:58 +0200

    first test site.conf for l2tp within the ffka network

--- site.mk
+++ site.mk
@@ -8,23 +8,24 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
+    gluon-config-mode-tunneldigger \
+    gluon-migrate-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-wifi-config \
-        gluon-luci-private-wifi \
+    gluon-luci-private-wifi \
 	gluon-next-node \
-	gluon-mesh-vpn-fastd \
+    gluon-mesh-vpn-tunneldigger \
 	gluon-setup-mode \
 	gluon-status-page \
 	haveged \
 	iptables \
 	iwinfo
 
-DEFAULT_GLUON_RELEASE := 0.2.70~stable$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.2.71~exp_turbo$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
